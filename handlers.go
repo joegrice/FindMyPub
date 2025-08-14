02133d3eedb94b91e5fc2030519f17c720630ec8
Joe Grice <joe.grice@hotmail.co.uk>
2025-08-14 22:03:47 +0100

    Add get long lat endpoint and pass to places

--- handlers.go
+++ handlers.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
@@ -16,6 +20,45 @@ type PubWithDistance struct {
 	PhotoURL string   `json:"photo_url"`
 }
 
+func getPlacesInternal(client *GoogleMapsClient, lat, lng string) ([]PubWithDistance, error) {
+	placesResponse, err := client.NearbySearch(lat, lng, 1609, "bar")
+	if err != nil {
+		return nil, err
+	}
+
+	var pubsWithDistance []PubWithDistance
+
+	for _, place := range placesResponse.Results {
+		distanceResponse, err := client.DistanceMatrix(lat, lng, place.PlaceID, "walking", "imperial")
+		if err != nil {
+			return nil, err
+		}
+
+		if len(distanceResponse.Rows) > 0 && len(distanceResponse.Rows[0].Elements) > 0 {
+			var photoURL string
+			if len(place.Photos) > 0 {
+				var photoURLBuilder strings.Builder
+				photoURLBuilder.WriteString("https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=")
+				photoURLBuilder.WriteString(place.Photos[0].PhotoReference)
+				photoURLBuilder.WriteString("&key=")
+				photoURLBuilder.WriteString(client.apiKey)
+				photoURL = photoURLBuilder.String()
+			}
+
+			pubsWithDistance = append(pubsWithDistance, PubWithDistance{
+				Place:    place,
+				Name:     place.Name,
+				Vicinity: place.Vicinity,
+				Distance: distanceResponse.Rows[0].Elements[0].Distance.Text,
+				Duration: distanceResponse.Rows[0].Elements[0].Duration.Text,
+				Types:    place.Types,
+				PhotoURL: photoURL,
+			})
+		}
+	}
+	return pubsWithDistance, nil
+}
+
 func getPlaces(client *GoogleMapsClient) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		lat := r.URL.Query().Get("lat")
@@ -26,45 +69,92 @@ func getPlaces(client *GoogleMapsClient) http.HandlerFunc {
 			return
 		}
 
-		placesResponse, err := client.NearbySearch(lat, lng, 1609, "bar")
+		pubs, err := getPlacesInternal(client, lat, lng)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		var pubsWithDistance []PubWithDistance
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(pubs)
+	}
+}
 
-		for _, place := range placesResponse.Results {
-			distanceResponse, err := client.DistanceMatrix(lat, lng, place.PlaceID, "walking", "imperial")
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
+func getLocationFromIP(ip string) (*Location, error) {
+	// For testing purposes, since localhost will resolve to ::1 or 127.0.0.1
+	if ip == "::1" || ip == "127.0.0.1" {
+		ip = "8.8.8.8" // Google's DNS
+		log.Printf("Using test IP address: %s", ip)
+	}
 
-			if len(distanceResponse.Rows) > 0 && len(distanceResponse.Rows[0].Elements) > 0 {
-				var photoURL string
-				if len(place.Photos) > 0 {
-					var photoURLBuilder strings.Builder
-					photoURLBuilder.WriteString("https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=")
-					photoURLBuilder.WriteString(place.Photos[0].PhotoReference)
-					photoURLBuilder.WriteString("&key=")
-					photoURLBuilder.WriteString(client.apiKey)
-					photoURL = photoURLBuilder.String()
-				}
+	resp, err := http.Get("http://ip-api.com/json/" + ip)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
 
-				pubsWithDistance = append(pubsWithDistance, PubWithDistance{
-					Place:    place,
-					Name:     place.Name,
-					Vicinity: place.Vicinity,
-					Distance: distanceResponse.Rows[0].Elements[0].Distance.Text,
-					Duration: distanceResponse.Rows[0].Elements[0].Duration.Text,
-					Types:    place.Types,
-					PhotoURL: photoURL,
-				})
-			}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("ip-api.com response: %s", string(body))
+
+	var location struct {
+		Lat float64 `json:"lat"`
+		Lon float64 `json:"lon"`
+	}
+
+	if err := json.Unmarshal(body, &location); err != nil {
+		return nil, err
+	}
+
+	return &Location{Lat: location.Lat, Lng: location.Lon}, nil
+}
+
+func getLocation(w http.ResponseWriter, r *http.Request) {
+	ip := r.Header.Get("X-Forwarded-For")
+	if ip == "" {
+		ip, _, _ = net.SplitHostPort(r.RemoteAddr)
+	}
+
+	log.Printf("IP address: %s", ip)
+
+	location, err := getLocationFromIP(ip)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(location)
+}
+
+func getPlacesNearMe(client *GoogleMapsClient) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ip := r.Header.Get("X-Forwarded-For")
+		if ip == "" {
+			ip, _, _ = net.SplitHostPort(r.RemoteAddr)
+		}
+
+		log.Printf("IP address: %s", ip)
+
+		location, err := getLocationFromIP(ip)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		lat := strconv.FormatFloat(location.Lat, 'f', -1, 64)
+		lng := strconv.FormatFloat(location.Lng, 'f', -1, 64)
+
+		pubs, err := getPlacesInternal(client, lat, lng)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 
 		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(pubsWithDistance)
+		json.NewEncoder(w).Encode(pubs)
 	}
 }
--- main.go
+++ main.go
@@ -9,13 +9,15 @@ import (
 )
 
 const (
-	apiKey = "AIzaSyAUhGplazFYisHpANo2Jr1jzK6aO2tFwTo"
+	apiKey = ""
 )
 
 func main() {
 	r := mux.NewRouter()
 	client := NewGoogleMapsClient(apiKey)
 	r.HandleFunc("/places", getPlaces(client))
+	r.HandleFunc("/location", getLocation)
+	r.HandleFunc("/places/near-me", getPlacesNearMe(client))
 	fmt.Println("Server listening on port 8080")
 	log.Fatal(http.ListenAndServe("localhost:8080", r))
 }
